331cb19b4cdf2ecb032b733b0bb35fd806dde80a
KwongSunny <sunnykwong91500@gmail.com>
2021-01-04 14:58:14 -0500

    Setting up

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('PixelBot, online!');
+})
+
+
+
+
+
+
+
+
+
+client.login('Nzk1NzM2MDg0MzI3MzAxMTYw.X_Ns-g.H4LodYb97C-XsVLQuKCRR5qhDzc');
+
