f1cf60c11d99d812041b26dec085ea1695c8a7b0
KwongSunny <sunnykwong91500@gmail.com>
2021-01-05 22:04:18 -0500

    testing kill

--- commands/kill.js
+++ commands/kill.js
@@ -1,9 +1,12 @@
 module.exports = {
     name: 'kill',
     description: "stops the bot",
-    execute(message, args){
+    async execute(message, args){
         if(message.author.id === '184783523624910858')
-            message.channel.send('Stopping processes...').then(args.destroy()).catch(console.error);
+        {
+            message.channel.send('Stopping processes...');
+            await args.destroy().catch(console.error);
+        }
         else
             message.channel.send('You have insufficient permissions to use this command.');
     }
