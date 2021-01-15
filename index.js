f07901c58a1784e91b512ae3e2f401c7fc76a852
Sunny Kwong <sunnykwong91500@gmail.com>
2021-01-15 16:42:20 -0500

    Token fix

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ client.on('messageReactionRemove', async(reaction, user) => {
     }
 });
 
-let deploy = 'LOCAL';
+let deploy = 'HEROKU';
 
 if(deploy === 'HEROKU') client.login(process.env.BOT_TOKEN);  //HEROKU PUBLIC BUILD 
 else{
